bafcdaeeb7f5d3097ec9aca8b8a10b316223878c
Kajal-Gupta23 <kjlgupta9503@gmail.com>
2023-07-21 20:33:30 +0530

    added serializer and created pricingconfigurations crud in views

--- pricing_app/views.py
+++ pricing_app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import PricingConfiguration
+from .serializers import PricingConfigurationSerializer
 
-# Create your views here.
+class PricingConfigurationAPI(APIView):
+    def get(self, request, format=None):
+        configurations = PricingConfiguration.objects.all()
+        serializer = PricingConfigurationSerializer(configurations, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = PricingConfigurationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk, format=None):
+        configuration = PricingConfiguration.objects.get(pk=pk)
+        serializer = PricingConfigurationSerializer(configuration, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        configuration = PricingConfiguration.objects.get(pk=pk)
+        configuration.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
