9d8c24cd481dda6de86548924ec7b9c6c4753464
Kajal-Gupta23 <kjlgupta9503@gmail.com>
2023-07-21 20:37:43 +0530

    added calculateprice class and its url

--- pricing_app/urls.py
+++ pricing_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import PricingConfigurationAPI
+from .views import PricingConfigurationAPI, CalculatePriceAPI
 
 urlpatterns = [
     path('api/pricing_configurations/', PricingConfigurationAPI.as_view(), name='pricing_configurations'),
-
+    path('api/calculate_price/', CalculatePriceAPI.as_view(), name='calculate_price'),
 ]
--- pricing_app/views.py
+++ pricing_app/views.py
@@ -29,3 +29,27 @@ class PricingConfigurationAPI(APIView):
         configuration = PricingConfiguration.objects.get(pk=pk)
         configuration.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CalculatePriceAPI(APIView):
+    def post(self, request, format=None):
+        try:
+            distance = float(request.data.get('distance', 0))
+            time_duration = float(request.data.get('time_duration', 0))
+            waiting_time = float(request.data.get('waiting_time', 0))
+
+            day_of_week = request.data.get('day_of_week', None)  
+            pricing_config = PricingConfiguration.objects.filter(day_of_week=day_of_week, is_active=True).first()
+
+            if pricing_config:
+                distance_base_price = float(pricing_config.distance_base_price)
+                distance_additional_price = float(pricing_config.distance_additional_price)
+                time_multiplier_factor = float(pricing_config.time_multiplier_factor)
+                waiting_charges = float(pricing_config.waiting_charges)
+
+                price = (distance_base_price + (max(float(distance) - 3, 0) * distance_additional_price)) + (float(time_duration) * time_multiplier_factor) + (float(waiting_time) * waiting_charges)
+                return Response({'price': price}, status=status.HTTP_200_OK)
+            else:
+                return Response({'error': 'No active pricing configuration found for the provided day.'}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
