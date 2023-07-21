ef0a192877aad5f5441fa5b5d81123f782531139
Kajal-Gupta23 <kjlgupta9503@gmail.com>
2023-07-21 20:40:01 +0530

    updated the model to accept only days from a week

--- pricing_app/models.py
+++ pricing_app/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 class PricingConfiguration(models.Model):
+    DAY_CHOICES = [
+    ('Mon', 'Monday'),
+    ('Tue', 'Tuesday'),
+    ('Wed', 'Wednesday'),
+    ('Thu', 'Thursday'),
+    ('Fri', 'Friday'),
+    ('Sat', 'Saturday'),
+    ('Sun', 'Sunday'),
+    ]
     distance_base_price = models.DecimalField(max_digits=8, decimal_places=2)
     distance_additional_price = models.DecimalField(max_digits=8, decimal_places=2)
     time_multiplier_factor = models.DecimalField(max_digits=8, decimal_places=2)
     waiting_charges = models.DecimalField(max_digits=8, decimal_places=2)
-    day_of_week = models.CharField(max_length=3)  # Assuming 'Mon', 'Tue', etc.
+    day_of_week = models.CharField(max_length=3, choices=DAY_CHOICES)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
