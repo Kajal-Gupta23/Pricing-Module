6af4b1d6aa5fce283b0bf896f05f55bd1acddfce
Kajal-Gupta23 <kjlgupta9503@gmail.com>
2023-07-21 21:17:04 +0530

    updated readme

--- pricing_app/admin.py
+++ pricing_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import PricingConfiguration
 
-# Register your models here.
+admin.site.register(PricingConfiguration)
+
+
--- pricing_app/urls.py
+++ pricing_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PricingConfigurationAPI, CalculatePriceAPI
 
 urlpatterns = [
-    path('api/pricing_configurations/', PricingConfigurationAPI.as_view(), name='pricing_configurations'),
-    path('api/calculate_price/', CalculatePriceAPI.as_view(), name='calculate_price'),
+    path('pricing_configurations/', PricingConfigurationAPI.as_view(), name='pricing_configurations'),
+    path('calculate_price/', CalculatePriceAPI.as_view(), name='calculate_price'),
 ]
